f3bc02bd5c9e34d71bd52f5d490df50eb9d7315f
Zackdokong <66398105+Zackdokong@users.noreply.github.com>
2024-10-29 11:40:48 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function scrollToSection(id) {
     const section = document.getElementById(id);
     section.scrollIntoView({ behavior: 'smooth' });
 }
+
+
+function scrollToFeatured() {
+    const section = document.getElementById("featured-products");
+    const navBarHeight = document.querySelector("nav").offsetHeight;
+
+    // 네비게이션 바 높이만큼 추가하여 가려지지 않도록 스크롤
+    window.scrollTo({
+        top: section.offsetTop - navBarHeight,
+        behavior: "smooth"
+    });
+}
